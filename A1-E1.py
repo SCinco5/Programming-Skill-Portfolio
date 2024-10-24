732af390e6be2a83b4bd7fd8aae04f85e54b51d6
SCinco5 <cinco.sofia2022@gmail.com>
2024-10-24 11:04:30 +0400

    Add files via upload

---
+++ A1-E1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 20:09:56 2024
+
+@author: Sofia Cinco
+"""
+
+word1= "Coding" #input the variables written from the exercise
+word2= "is"
+word3= "Cool"
+
+
+print(word1,""+word2,"" +word3) #since it mentioned to make string 
+#concatenation, (string=letters) so with that I typed in (word1,word2,word3)
+#to make it easier if I ever wrote something long.
+#I then added (" ")to put spaces between the words,
+#then added + to make it in one line.
