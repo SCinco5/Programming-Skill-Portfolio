96195929989866af1094c046714b718a9a7e2f40
SCinco5 <cinco.sofia2022@gmail.com>
2024-10-24 11:32:01 +0400

    Add files via upload

---
+++ A1-E2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 10:23:06 2024
+
+@author: Sofia Cinco
+"""
+
+
+
+num1=8 #first declare the variable then add the integer after
+num2=10
+result=num1+num2 #then input the third variable to make the result
+#by adding num1 and num2 to make it more time efficient
+print("SUM=",result) #lastly input the function print() for it to be
+#displayed in the console
+
